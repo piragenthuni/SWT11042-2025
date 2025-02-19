cf253ff7909b5c92275f9ef4fae5100a4b83cfe2
piragenthuni <piragenthuni@gmail.com>
2025-02-19 09:25:02 +0530

    made first code

--- second.c
+++ second.c
@@ -1,7 +1,7 @@
-
-include <#stdio.h>
+#include <stdio.h>
 
 int main(){
-printf("welcome to the fundamentals of programming session\n");
-  return 0;
+    printf("welcome to the fundamentals of programming session\n");
+    printf("Piragenth Satkunananthan");
+    return 0;
 }
